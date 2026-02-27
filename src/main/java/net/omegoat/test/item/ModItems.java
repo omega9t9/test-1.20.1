ba1c967a9e8063a539d1cab17056ff8957b9fa37
omegoat <amanverma9t9@gmail.com>
2026-02-27 10:49:19 +0530

    0.2.0

--- src/main/java/net/omegoat/test/item/ModItems.java
+++ src/main/java/net/omegoat/test/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.omegoat.test.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,6 +10,7 @@ import net.omegoat.test.Test;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Test.MOD_ID, name), item);
@@ -15,5 +18,10 @@ public class ModItems {
 
     public static void registerModItems() {
         Test.LOGGER.info("Registering Mod items " + Test.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
+        });
     }
 }
